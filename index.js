0a4bcc77f1e4a288f5a5b57dc665534d10fa3fb1
Devin44G <35251347+Devin44G@users.noreply.github.com>
2020-03-09 16:29:37 -0500

    Created GET and POST endpoints for user

--- index.js
+++ index.js
@@ -1,6 +1,47 @@
 const express = require('express');
+const shortid = require('shortid');
 
 const server = express();
+      server.use(express.json());
+
+/********* ENDPOINT VARS *********/
+let users = [];
+
+/********* ENDPOINTS *********/
+server.get('/api/users', (req, res) => {  // GET
+  if(!req) {
+    res.status(500).json({ errorMsg: "THere was an error while saving the user to the database." });
+  } else {
+    res.status(200).json(users);
+  }
+});
+
+server.post('/api/users/', (req, res) => {
+  const userInfo = req.body;
+        userInfo.id = shortid.generate();
+        users.push(userInfo);
+
+  if(!req.body.name && !req.body.bio) {
+    res.status(400).json({ errorMsg: "Please provide a name and bio for the users." });
+  } else if(!req) {
+    res.status(500).json({ errorMsg: "THere was an error while saving the user to the database." });
+  } else {
+    res.status(201).json(userInfo);
+  }
+});
+
+server.get('api/users/:id', (req, res) => {
+  const {id} = req.params;
+  const user = users.find(id === String(id));
+
+  if(!user) {
+    res.status(404).json({ message: "The user with the specified ID does not exist." });
+  } else if(!id) {
+    res.status(500).json({ errorMessage: "The user information could not be retrieved." });
+  } else {
+    res.status(200).json(user);
+  }
+});
 
 const PORT = 5000;
 
